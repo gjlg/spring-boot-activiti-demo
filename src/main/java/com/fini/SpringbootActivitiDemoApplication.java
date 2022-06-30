ddcfcdcd261538f27b5aa70497f2b7e5c1c0567b
yun_kai <yun_kai@163.com>
2022-06-30 18:25:49 +0800

    create bpmn2.0 file

---
+++ src/main/java/com/fini/SpringbootActivitiDemoApplication.java
@@ -0,0 +1,13 @@
+package com.fini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootActivitiDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootActivitiDemoApplication.class, args);
+    }
+
+}
